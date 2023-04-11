aea6146057776a9dea41863bcc178ff6db158637
sanchit-gandhi <sanchit@huggingface.co>
2023-04-11 15:29:45 +0100

    better pip install

--- setup.py
+++ setup.py
@@ -1,4 +1,69 @@
+# Copyright 2023 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+
+# To use a consistent encoding
+from codecs import open
+
 from setuptools import find_packages, setup
 
+_deps = [
+    "transformers>=4.27.4",
+    "flax",
+]
 
-setup(name="whisper_jax", version="0.0.0", packages=find_packages())
+_extras_dev_deps = [
+    "black~=23.1",
+    "isort>=5.5.4",
+    "ruff>=0.0.241,<=0.0.259",
+]
+
+_extras_app_deps = [
+    "gradio>=3.25.0",
+    "requests>=2.28.2",
+    "fastapi>=0.95.0",
+    "uvicorn>=0.21.1",
+    "pytube>=12.1.3",
+]
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+# read version
+with open(os.path.join(here, "whisper_jax", "__init__.py"), encoding="utf-8") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.split("=")[1].strip().strip('"')
+            break
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+setup(
+    name="whisper_jax",
+    version=version,
+    description="JAX implementation of OpenAI's Whisper model.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=_deps,
+    extras_require={
+        "dev": [_extras_dev_deps],
+        "app": [_extras_app_deps],
+    },
+)
--- whisper_jax/layers.py
+++ whisper_jax/layers.py
@@ -1,4 +1,5 @@
-# Copyright 2022 The T5X Authors.
+# coding=utf-8
+# Copyright 2023 The T5X Authors and The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
